8487f80e52245806eb87b8a23d6165ceb74b1290
Giovanny Perez <789@holbertonschool.com>
2019-07-02 21:17:13 +0000

    Fix  New classes

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class user """
+from models.base_model import BaseModel
+
+email = ""
+password = ""
+first_name = ""
+last_name = ""
+
+class User(BaseModel):
+    """ User class """
+    def __init__(self, *args, **kwargs):
+        """ Init """
+        super().__init__(*args, **kwargs)
