7fabe051d9865909afdd93387edd799f1296c467
Jeniffervp <737@holbertonschool.com>
2019-07-03 23:56:46 +0000

    [Feature]test file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+import unittest
+import pep8
+from models.base_model.py import BaseModel
+from models.engine.file_storage import FileStorage
+from models.__init__ import storage
+
+def setUpModule():
+    """ """
+    pass
+
+
+def tearDownModule():
+    """ """
+    pass
+
+
+class TestStringMethods(unittest.TestCase):
+    """ Check the pep8 """
+    def testpep8(self):
+        style = pep8.StyleGuide(quiet=True)
+        file1 = "models/engine/file_storage.py"
+        file2 = "tests/test_models/test_engine/test_file_storage.py"
+        check = style.check_files([file1, file2])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warning).")
+
+
+class TestModels(unittest.TestCase):
+
+    def setUp(self):
+        """ Set a variable """
+        self.fisto = FileStorage()
+        self.my_model = BaseModel()
+        print("setUp")
+
+    def tearDown(self):
+        """ End variable """
+        print("tearDown")
+
+    @classmethod
+    def setUpClass(cls):
+        """ Set a Class """
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Del a Class"""
+        print("tearDownClass")
+
+    def test_file_storage_doc(self):
+        """ Check the documentation """
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.__init__.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    def test_fiel_storage_exist(self):
+        """ Check if methods exists """
+        self.assertTrue(hasattr(self.fisto, "__init__"))
+        self.assertTrue(hasattr(self.fisto, "all"))
+        self.assertTrue(hasattr(self.fisto, "new"))
+        self.assertTrue(hasattr(self.fisto, "save"))
+        self.assertTrue(hasattr(self.fisto, "reload"))
+
+if __name__ == '__main__':
+    unittest.main()
